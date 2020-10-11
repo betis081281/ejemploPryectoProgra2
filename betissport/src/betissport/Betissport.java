c4b67f82a6e45bc8eb675a2525bed00e5296c3a8
betis081281 <guatebetico@gmail.com>
2020-10-11 08:53:03 -0600

    Carga inicial
    Carga inicial del proyecto betissport

---
+++ betissport/src/betissport/Betissport.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package betissport;
+
+/**
+ *
+ * @author Ismael Ruiz
+ */
+public class Betissport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu ej = new Menu();
+        ej.setLocationRelativeTo(null);
+        ej.setVisible(true);
+    }
+
+}
